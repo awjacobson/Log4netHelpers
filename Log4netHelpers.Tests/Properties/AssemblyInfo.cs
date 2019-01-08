7265c0db3198f74654fa47e101c910ee1419f1c5
Aaron Jacobson <ajacobson@teamhuber.com>
2019-01-08 15:44:29 -0600

    Adding Test project

---
+++ Log4netHelpers.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Log4netHelpers.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Log4netHelpers.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("22b5b1db-4e1f-4872-a850-114d675e0c99")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Log4netHelpers.Tests/ToDebugStringShould.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using Log4netHelpers.Extensions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Log4netHelpers.Tests
+{
+    [TestClass]
+    public class ToDebugStringShould
+    {
+        [DataTestMethod]
+        [DataRow(null, "null")]
+        [DataRow(new string[] { "Text1" }, "[\"Text1\"]")]
+        [DataRow(new string[] { "Text1", "Text2", "Text3" }, "[\"Text1\",\"Text2\",\"Text3\"]")]
+        public void HanldeIEnumerableString(IEnumerable<string> testObject, string expected)
+        {
+            // Act
+            var actual = testObject.ToDebugString();
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
